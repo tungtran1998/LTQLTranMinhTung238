e071d2fd9bff9c6c29e2185a5627ae5ff2503b4d
tung tran <trantung1238@gmail.com>
2021-12-21 13:08:36 +0700

    Add project files.

---
+++ LTQL_1621050238/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LTQL_1621050238
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LTQL_1621050238/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LTQL_1621050238.MvcApplication" Language="C#" %>
